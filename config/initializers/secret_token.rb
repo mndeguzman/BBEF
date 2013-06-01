4a7c1c86f16ab7bab15d99c2e980dcacf19962f1
caoilte dunne <cdunne@dius.com.au>
2013-06-01 10:40:38 +1000

    First cut

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BBEF::Application.config.secret_token = '4ac6d0c6fb2c32f88dc97626756a7ed1e9fc7efe49a6bffda641decdb9387297811dedac75bdd9565314c3102d93544313b8c838344ff2aef4c766ec3f75b969'
